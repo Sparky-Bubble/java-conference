30fdbcc389893f98f4d4a1cb55159dbd65f3689d
lihuang-zheng <jerryzheng0131@gmail.com>
2022-03-08 11:22:33 -0600

    base structure for java conference.

---
+++ src/main/java/com/sparkybubble/javaconference/JavaconferenceApplication.java
@@ -0,0 +1,13 @@
+package com.sparkybubble.javaconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaconferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaconferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparkybubble/javaconference/JavaconferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkybubble.javaconference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaconferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
