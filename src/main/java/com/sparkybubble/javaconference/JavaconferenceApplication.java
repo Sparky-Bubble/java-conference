78a7c3160f98b666875a42218405a2862259654e
lihuang-zheng <jerryzheng0131@gmail.com>
2022-03-11 05:29:20 -0600

    Reorganized the code and refined the output.

--- src/main/java/com/sparkybubble/javaconference/JavaconferenceApplication.java
+++ src/main/java/com/sparkybubble/javaconference/JavaconferenceApplication.java
@@ -21,38 +21,52 @@ public class JavaconferenceApplication {
 			track.add(sortTalk(file));
 		}
 
+		System.out.println();
+		System.out.println("========================  // Java Conference // ========================");
+		System.out.println();
+
 		//Figure out a real display
 		int trackNum = 1;
-		for(List<String> list : track){
+		for(List<String> list : track) {
 			System.out.println("Track " + trackNum + ":");
-			for(String s : list){
+
+			for(String s : list) {
 				System.out.println(s);
 			}
+
 			trackNum++;
 		}
 	}
-	public static HashMap<String, Integer> readFile(){
+
+
+	public static HashMap<String, Integer> readFile() {
 		HashMap<String, Integer> input = new HashMap<>();
 		String talk; int duration;
-		try{
+
+		try {
 			File file = new File("src/main/resources/text.txt");
 			Scanner scan = new Scanner(file);
-			while(scan.hasNextLine()){
+
+			while(scan.hasNextLine()) {
 				String line = scan.nextLine();
 				talk = line.substring(0, line.lastIndexOf(" "));
-				if(line.contains("lightning")){
+
+				if(line.contains("lightning")) {
 					duration = 5;
-				}else {
+				} else {
 					duration = Integer.parseInt(line.substring(line.lastIndexOf(" ")+1, line.lastIndexOf(" ") + 3));
 				}
 				input.put(talk, duration);
 			}
-		}catch(Exception e){
+		}catch(Exception e) {
 			System.out.println(e);
 		}
+
 		return input;
 	}
-	public static ArrayList<String> sortTalk(HashMap<String, Integer> talks){
+
+
+	public static ArrayList<String> sortTalk(HashMap<String, Integer> talks) {
 		Set<String> removed = new HashSet<>();
 		ArrayList<String> sorted = new ArrayList<>();
 		int morning = 180, afternoon = 240;
@@ -63,18 +77,19 @@ public class JavaconferenceApplication {
 		Iterator morningIt = talks.entrySet().iterator();
 
 		//while loop for the morning slots
-		while(morningIt.hasNext()){
+		while(morningIt.hasNext()) {
 
-			if(morning == 0){
+			if(morning == 0) {
 				break;
 			}
+
 			Map.Entry<String,Integer> event = (Map.Entry<String, Integer>) morningIt.next();
 			if(event.getValue() <= morning) {
 				//if statement checks to see whether the talk being checked can fit into the slot
 				String time = morningSlot.format(
 						DateTimeFormatter.ofLocalizedTime( FormatStyle.SHORT )
 								.withLocale( Locale.US ));
-				sorted.add(time + " " + event.getKey() + " " + event.getValue() + "mins");
+				sorted.add(time + " " + event.getKey() + "   " + event.getValue() + " min");
 				morningSlot = morningSlot.plusMinutes(event.getValue());
 				morning -= event.getValue();
 				removed.add((String)event.getKey());
@@ -87,18 +102,20 @@ public class JavaconferenceApplication {
 
 		sorted.add("12:00 PM LUNCH");
 		Iterator afternoonIt = talks.entrySet().iterator();
+
 		//while loops for the afternoon slots
 		while(afternoonIt.hasNext()){
 			if(afternoon == 0){
 				break;
 			}
+
 			Map.Entry<String, Integer> event = (Map.Entry<String, Integer>) afternoonIt.next();
 			if(event.getValue() <= afternoon) {
 				//if statement checks to see whether the talk being checked can fit into the slot
 				String time = afternoonSlot.format(
 						DateTimeFormatter.ofLocalizedTime( FormatStyle.SHORT )
 								.withLocale( Locale.US ));
-				sorted.add(time + " " + event.getKey() + " " + Integer.toString(event.getValue()) + "mins");
+				sorted.add(time + " " + event.getKey() + "    " + event.getValue() + " min");
 				afternoonSlot = afternoonSlot.plusMinutes(event.getValue());
 				afternoon -= event.getValue();
 				removed.add(event.getKey());
